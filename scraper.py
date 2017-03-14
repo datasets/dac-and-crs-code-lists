efc4f8ba44555ede40e373bd0a259e32f35105b8
Andy Lulham <a.lulham@gmail.com>
2017-03-14 22:20:57 +0000

    Bring original code up to date a bit

--- scraper.py
+++ scraper.py
@@ -1,103 +1,118 @@
-"""
-Generates codelist files from CRS codelists.
-Note that it doesn't handle removed / historical codes.
-"""
+import csv
+import json
+from os.path import join
 
-from lxml import etree as ET
+from bs4 import BeautifulSoup as bs
+import requests
+from lxml import etree
 import xlrd
-import crslib
-crs_mappings = crslib.CRS_MAPPINGS
-from crslib import make_str
+
+
+'''
+Generates codelist files from CRS codelists.
+'''
+
+with open('crs_mappings.json') as f:
+    crs_mappings = json.load(f)
+
+base_url = 'http://www.oecd.org'
+
+codelists_url = base_url + '/dac/stats/dacandcrscodelists.htm'
+r = requests.get(codelists_url)
+soup = bs(r.text, 'lxml')
+
+xls_filepath = join('output', 'DAC-CRS-CODES.xls')
+xls_url = soup.find(class_='document').find('a')['href']
+if xls_url.startswith('/'):
+    xls_url = base_url + xls_url
+
+
+with open(xls_filepath, 'wb') as f:
+    r = requests.get(xls_url, stream=True)
+    if not r.ok:
+        raise
+    for block in r.iter_content(1024):
+        _ = f.write(block)
 
 # Open CRS codelist
-crs_xls = xlrd.open_workbook("source/CRS-codelists.xls")
+crs_xls = xlrd.open_workbook(xls_filepath)
 
 def get_crs_codelist(book, mapping):
-    def relevant_row(mapping, rowdata):
-        for eb in mapping["exclude_blank"]:
-            if str(rowdata[eb]).strip() == "": return False
+    def to_str(val):
+        if type(val) is float:
+            if val == val // 1:
+                return str(int(val))
+        return str(val).strip()
+
+    def relevant_row(mapping, row_data):
+        for eb in mapping['exclude_blank']:
+            if str(row_data[eb]).strip() == '': return False
         return True
 
     # Get sheet and create array for this codelist
-    cl = book.sheet_by_name(mapping["sheet_name"])
+    cl = book.sheet_by_name(mapping['sheet_name'])
     cldata = []
 
     # Some values apply to following rows; we save them in `fill_down_vals`
     fill_down_vals = {}
-    for i in range (mapping["start_row"], cl.nrows):
-
+    for row_num in range(mapping['start_row'], cl.nrows):
         # Map columns to values
-        rowdata = {}
-        for j in mapping["cols"].keys():
-            rowdata[mapping["cols"][j]] = make_str(cl.cell_value(i,j))
+        row_data = {}
+        for col_nums, col_name in mapping['cols']:
+            if type(col_nums) is not list:
+                col_nums = [col_nums]
+
+            # if we're merging multiple columns,
+            # take the first one that isn't blank
+            for col_num in col_nums:
+                row_data[col_name] = to_str(cl.cell_value(row_num, col_num))
+                if row_data[col_name].strip() != '':
+                    break
+
+        # if the value for a given column is in `ignore`
+        # then ignore this row
+        skip_row = False
+        for ig_key, ig_val in mapping.get('ignore', []):
+            if row_data[ig_key] == ig_val:
+                skip_row = True
+                break
+        if skip_row:
+            continue
 
         # Check whether we should ignore this row based on `exclude_blank`
-        if relevant_row(mapping, rowdata):
+        if relevant_row(mapping, row_data):
             if fill_down_vals:
-                rowdata.update(fill_down_vals)
-            cldata.append(rowdata)
-        else:
-            if mapping.get("fill_down") and (rowdata[mapping["fill_down"]] != ""):
-                fill_down_vals[mapping["fill_down"]] = rowdata[mapping["fill_down"]]
+                row_data.update(fill_down_vals)
+            cldata.append(row_data)
+
+        if mapping.get('fill_down') and (row_data[mapping['fill_down']] != ''):
+            fill_down_vals[mapping['fill_down']] = row_data[mapping['fill_down']]
 
     return cldata
 
-codelists = {}
 for name, mapping in crs_mappings.items():
-    print "Getting mapping %s" % name
-    codelists[name] = get_crs_codelist(crs_xls, mapping)
+    print('Getting mapping {}'.format(name))
+    codelist = get_crs_codelist(crs_xls, mapping)
+    with open(join('output', name + '.csv'), 'w') as f:
+        fieldnames = [c[1] for c in mapping['cols']]
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for c in codelist:
+            _ = writer.writerow(c)
 
 # Purpose codes reformatting to combine two sheets
-pcfr = dict(map(lambda x: (x["code"], x), codelists["SectorFR"]))
-for key, row in enumerate(codelists["Sector"]):
-    frcodes = pcfr[row["code"]]
-    codelists["Sector"][key].update(frcodes)
-codelists.pop("SectorFR", None)
+sectors = {}
+for lang in ['en', 'fr']:
+    with open(join('output', 'sector_{}.csv'.format(lang))) as f:
+        reader = csv.DictReader(f)
+        sectors[lang] = [row for row in reader]
+fr_lookup = {row['code']: row for row in sectors['fr']}
+for row in sectors['en']:
+    row.update(fr_lookup[row['code']])
 
-# Finished collecting data from spreadsheet, now start writing XML
-
-# Write for each template
-for codelist_name, data in codelists.items():
-    print "Writing codelist for %s" % codelist_name
-    template = ET.parse('templates/%s.xml' % codelist_name, ET.XMLParser(remove_blank_text=True))
-    codelist_items = template.find('codelist-items')
-    for code_data in data:
-        codelist_item = ET.Element('codelist-item')
-
-        code = ET.Element('code')
-        code.text = code_data["code"]
-        codelist_item.append(code)
-
-        name = ET.Element('name')
-        codelist_item.append(name)
-
-        narrative_en = ET.Element('narrative')
-        narrative_en.text = code_data["name_en"]
-        name.append(narrative_en)
-
-        narrative_fr = ET.Element('narrative')
-        narrative_fr.set('{http://www.w3.org/XML/1998/namespace}lang', "fr")
-        narrative_fr.text = code_data["name_fr"]
-        name.append(narrative_fr)
-
-        if code_data.get("description_en"):
-            description = ET.Element('description')
-            codelist_item.append(description)
-
-            narrative_en = ET.Element('narrative')
-            narrative_en.text = code_data["description_en"]
-            description.append(narrative_en)
-
-            narrative_fr = ET.Element('narrative')
-            narrative_fr.set('{http://www.w3.org/XML/1998/namespace}lang', "fr")
-            narrative_fr.text = code_data["description_fr"]
-            description.append(narrative_fr)
-
-        if code_data.get("category"):
-            category = ET.Element('category')
-            category.text = code_data["category"]
-            codelist_item.append(category)
-
-        codelist_items.append(codelist_item)
-    crslib.reindent(template)
-    template.write('xml/%s.xml' % codelist_name, pretty_print=True, encoding="UTF-8", xml_declaration=True)
+fieldnames = ['category', 'code', 'name_en', 'description_en', 'name_fr', 'description_fr']
+with open(join('output', 'sector.csv'), 'w') as f:
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    writer.writeheader()
+    for c in sectors['en']:
+        _ = writer.writerow(c)
