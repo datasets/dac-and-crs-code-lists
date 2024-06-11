33178afe2c69cc7fd7caa396cc5556f20551e783
Mark Brough <mark.brough@gmail.com>
2024-06-11 14:38:21 +0200

    Get XML Lookup URL working while avoiding Cloudflare

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ from scrapers.excel import scrape_excel
 from scrapers.xml import scrape_xml
 from parsers.excel import parse_excel
 
-#scrape_xml("source/codelists.xml")
+scrape_xml("source/codelists.xml")
 scrape_excel("source/codelists.xls")
 
 parse_excel("source/codelists.xls", "data")
--- scrapers/xml.py
+++ scrapers/xml.py
@@ -4,19 +4,7 @@ from .helpers import fetch_html, rel_to_absolute, save_from_url
 
 
 def scrape_xml(xml_filepath):
-    url = 'http://www.oecd.org/dac/stats/dacandcrscodelists.htm'
-
-    base_url = urlunparse(urlparse(url)._replace(path=''))
-    soup = fetch_html(url)
-    doc = soup.find(class_='document')
-    xml_page_url = doc.find('a', text='XML')['href']
-    xml_page_url = rel_to_absolute(xml_page_url, base_url=base_url)
-
-    base_url = urlunparse(urlparse(xml_page_url)._replace(path=''))
-    soup = fetch_html(xml_page_url)
-    doc = soup.find(class_='document')
-    xml_lookup_url = doc.find('a', text='DAC codelist in XML format')['href']
-    xml_lookup_url = rel_to_absolute(xml_lookup_url, base_url=base_url)
+    xml_lookup_url = 'https://webfs.oecd.org/crs-iati-xml/Lookup/'
 
     base_url = urlunparse(urlparse(xml_lookup_url)._replace(path=''))
     soup = fetch_html(xml_lookup_url)
