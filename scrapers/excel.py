aaf89a29939142bae2cc5b2b20aba3e90bd0a44d
Mark Brough <mark.brough@gmail.com>
2025-01-08 15:37:01 +0100

    Update scraper

--- scrapers/excel.py
+++ scrapers/excel.py
@@ -8,11 +8,9 @@ def scrape_excel(xls_filepath):
     '''
     Generate codelist files from XLS CRS codelists.
     '''
-    url = 'https://webcache.googleusercontent.com/search?q=cache:https://www.oecd.org/dac/financing-sustainable-development/development-finance-standards/dacandcrscodelists.htm'
+    url = 'https://webfs.oecd.org/oda/DataCollection/Resources/'
     base_url = urlunparse(urlparse(url)._replace(path=''))
     soup = fetch_html(url)
-    doc = soup.find(class_='document')
-    # XLS link sometimes includes a space
-    xls_url = doc.find('a', string=re.compile(r'(\s*)XLS(\s*)'))['href']
+    xls_url = soup.find('a', string=re.compile(r'(\s*)DAC-CRS-CODES.xls'))['href']
     xls_url = rel_to_absolute(xls_url, base_url=base_url)
     save_from_url(xls_url, xls_filepath)
